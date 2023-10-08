2ecbe910192c8c33a8e3b9856e2fae9adc134d76
grzeg2010 <grzeg2010@gmail.com>
2023-10-08 17:55:37 +0200

    Check if a and b are greater than 0, fix Scanner

--- gs_euclids_algorithm/EuclidsAlgorithm.java
+++ gs_euclids_algorithm/EuclidsAlgorithm.java
@@ -4,21 +4,24 @@ public class EuclidsAlgorithm
 {
     public static void main(String[] args)
     {
-        Scanner getNumber = new Scanner;
+        Scanner getNumber = new Scanner(System.in);
         int a = 48, b = 18;
 
         System.out.println("Podaj liczbę " + "a: ");
         a = getNumber.nextInt();
-        System.out.println("Podaj liczbę " + "b: ")
+        System.out.println("Podaj liczbę " + "b: ");
         b = getNumber.nextInt();
 
-        while (a != b)
+        if (a > 0 && b > 0)
         {
-            if (a > b)
-                a -= b;
-            else
-                b -= a;
+            while (a != b)
+            {
+                if (a > b)
+                    a -= b;
+                else
+                    b -= a;
+            }
+            System.out.println("Największy wspólny dzielnik:\n" + a);
         }
-        System.out.println("Największy wspólny dzielnik:\n" + a);
     }
 }
