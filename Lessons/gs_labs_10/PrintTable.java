cf46e0c44a3fc259964c78e4ae223dfab5cee642
grzeg2010 <grzeg2010@gmail.com>
2023-12-06 21:52:57 +0100

    Finish ex 2 from lesson 10

--- Lessons/gs_labs_10/PrintTable.java
+++ Lessons/gs_labs_10/PrintTable.java
@@ -1,11 +1,21 @@
 public class PrintTable
 {
-    public static void prTab(int tab[][])
+    public static void print1D(int tab[])
+    {
+        for(int i = 0; i < tab.length; i++)
+        {
+            System.out.print(tab[i] + " ");
+        }
+
+        System.out.println("\n----------");
+    }
+
+    public static void print2D(int tab[][])
     {
         for(int i = 0; i < tab.length; i++)
         {
             for(int j = 0; j < tab[i].length; j++)
-                System.out.print(tab[i][j]);
+                System.out.print(tab[i][j] + " ");
 
             System.out.println();
         }
---
+++ Lessons/gs_labs_10/Zad2.java
@@ -0,0 +1,62 @@
+public class Zad2
+{
+    public static void main(String[] args)
+    {
+        int tabY = (int)(Math.random() * 10);
+        int tabX = (int)(Math.random() * 10) + 1;
+
+        int[][] tab = new int[tabY][tabX];
+
+        int rowLengths, newTabIndex;
+        int[][] newTab = new int[tabY][0];
+
+        // Fill array
+        for(int i = 0; i < tabY; i++)
+        {
+            for(int j = 0; j < tabX; j++)
+            {
+                tab[i][j] = (int)(Math.random() * 10);
+            }
+        }
+
+        PrintTable.print2D(tab);
+        
+        for(int i = 0; i < tabY; i++)
+        {
+            rowLengths = 0;
+
+            // Delete repeating elements
+            // and count unique elements (in rowLengths)
+            for(int j = 0; j < tabX; j++)
+            {
+                if(tab[i][j] != -1) // Reduces number of iterations
+                {
+                    for(int n = j + 1; n < tabX; n++)
+                    {
+                        if(tab[i][j] == tab[i][n])
+                            tab[i][n] = -1;  
+                    }
+
+                    rowLengths++;
+                }
+            }
+
+            // Set row length in target array
+            newTab[i] = new int[rowLengths];
+
+            // Fill new array row with unique elements
+            newTabIndex = 0;
+            for(int j = 0; j < tabX; j++)
+            {
+                if(tab[i][j] != -1)
+                {
+                    newTab[i][newTabIndex] = tab[i][j];
+                    newTabIndex++;
+                }
+            }
+        }
+
+        PrintTable.print2D(tab);
+        PrintTable.print2D(newTab);
+    }
+}
