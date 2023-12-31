5e6027bf8e7942e5c8a296d46fffe00dfbb8f1b0
grzeg2010 <grzeg2010@gmail.com>
2023-12-31 16:24:46 +0100

    Refactor instantiation in Main.java

--- PersonalProjects/gs_drones/src/Main.java
+++ PersonalProjects/gs_drones/src/Main.java
@@ -14,7 +14,10 @@ public class Main
     public static void main(String[] args)
     {
         // Dron ID: 1
-        Dron dronTestowy = new Dron((byte)1, "DronTestowy", 10d, 30d, 80d);
+        Dron dronTestowy = new Dron((byte)1,
+                "DronTestowy",
+                10d, 30d, 80d);
+
         Main.consoleOutputDivider(dronTestowy);
 
         System.out.println(dronTestowy.checkFlyParameters());
@@ -27,7 +30,10 @@ public class Main
         dronTestowy.revEngine();
 
         // Dron ID: 2
-        Dron dronTestowyCiezki = new Dron((byte)2, "CiezkiDron", 20d, 10d, 50d);
+        Dron dronTestowyCiezki = new Dron((byte)2,
+                "CiezkiDron",
+                20d, 10d, 50d);
+
         Main.consoleOutputDivider(dronTestowyCiezki);
 
         System.out.println(dronTestowyCiezki.checkFlyParameters());
@@ -44,19 +50,31 @@ public class Main
         RacingDrone[] raceDrones = new RacingDrone[3];
 
         // Dron ID: 3
-        RacingDrone dronWyscigowy1 = new RacingDrone((byte)3, "DronWyscigowy1", 10d, 50d, 80d, "RampolisRT", 7);
+        RacingDrone dronWyscigowy1 = new RacingDrone((byte)3,
+                "DronWyscigowy1",
+                10d, 50d, 80d,
+                "RampolisRT", 7);
+
         raceDrones[0] = dronWyscigowy1;
         Main.consoleOutputDivider(dronWyscigowy1);
         dronWyscigowy1.revEngine();
 
         // Dron ID: 4
-        RacingDrone dronWyscigowy2 = new RacingDrone((byte)4, "DronWyscigowy2", 12d, 58d, 80d, "Verne Energy", 5);
+        RacingDrone dronWyscigowy2 = new RacingDrone((byte)4,
+                "DronWyscigowy2",
+                12d, 58d, 80d,
+                "Verne Energy", 5);
+
         raceDrones[1] = dronWyscigowy2;
         Main.consoleOutputDivider(dronWyscigowy2);
         dronWyscigowy2.revEngine();
 
         // Dron ID: 5
-        RacingDrone dronWyscigowy3 = new RacingDrone((byte)5, "DronWyscigowy3", 11d, 48d, 80d, "Corigis Power", 4);
+        RacingDrone dronWyscigowy3 = new RacingDrone((byte)5,
+                "DronWyscigowy3",
+                11d, 48d, 80d,
+                "Corigis Power", 4);
+
         raceDrones[2] = dronWyscigowy3;
         Main.consoleOutputDivider(dronWyscigowy3);
         dronWyscigowy3.revEngine();
@@ -67,7 +85,9 @@ public class Main
 
         // VAMPIRE DRONES
         // Dron ID: 6
-        VampireDrone vamp1 = new VampireDrone((byte)6, 15d, 30d, 50d);
+        VampireDrone vamp1 = new VampireDrone((byte)6,
+                15d, 30d, 50d);
+
         Main.consoleOutputDivider(vamp1);
         System.out.println("Vampire drone energy before: " + vamp1.getValue("batteryLevel"));
         System.out.println("Pray drone energy before: " + dronWyscigowy2.getValue("batteryLevel"));
@@ -88,21 +108,30 @@ public class Main
 
         // CHRISTMAS DRONES
         // Dron ID: 7
-        ChristmasDrone christmasDrone1 = new ChristmasDrone((byte)7, "Delivery 1", teddyBear);
+        ChristmasDrone christmasDrone1 = new ChristmasDrone((byte)7,
+                "Delivery 1",
+                teddyBear);
+
         Main.consoleOutputDivider(christmasDrone1);
         christmasDrone1.deliverGift();
         teddyBear.prepare();
         christmasDrone1.deliverGift();
 
         // Dron ID: 8
-        ChristmasDrone christmasDrone2 = new ChristmasDrone((byte)8, "Delivery 2", toyCar);
+        ChristmasDrone christmasDrone2 = new ChristmasDrone((byte)8,
+                "Delivery 2",
+                toyCar);
+
         Main.consoleOutputDivider(christmasDrone2);
         christmasDrone2.deliverGift();
         christmasDrone2.deliverGift();
         toyCar.unpack();
 
         // Dron ID: 9
-        ChristmasDrone christmasDrone3 = new ChristmasDrone((byte)9, "Delivery 3", gardenToyHouse);
+        ChristmasDrone christmasDrone3 = new ChristmasDrone((byte)9,
+                "Delivery 3",
+                gardenToyHouse);
+
         Main.consoleOutputDivider(christmasDrone3);
         christmasDrone3.deliverGift();
         gardenToyHouse.unpack();
